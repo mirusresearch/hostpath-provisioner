d3b5f4a748ac22f304beac8520e514692fdb5095
Alec Troemel <alec@mirusresearch.com>
2017-03-07 14:05:53 -0600

    removed uneeded files, update glide.yaml

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # docker run --rm -it -v "$GOPATH":/gopath -v "$(pwd)":/app -e "GOPATH=/gopath" -w /app golang:1.4.2 sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o hostpath-provisioner'
 
-docker build -t alectroemel/hostpath-provisioner:$0 -t alectroemel/kube-cert-manager:latest .
-docker push alectroemel/hostpath-provisioner:$0
-docker push alectroemel/hostpath-provisioner:latest
+# docker build -t alectroemel/hostpath-provisioner:$0 -t alectroemel/kube-cert-manager:latest .
+# docker push alectroemel/hostpath-provisioner:$0
+# docker push alectroemel/hostpath-provisioner:latest
